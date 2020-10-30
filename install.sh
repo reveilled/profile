6e0af440848c136ae5fcc382965e9880d92148ee
reveilled <reveilled1@gmail.com>
2020-10-29 23:13:17 -0400

    Updates for Ubuntu 20.04

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp .gdbinit ~
 cp -r utils/* ~
 
 #machine setup (Fresh UBUNTU)
-sudo apt install vim htop python-pip python3-pip llvm libssl-dev cmake openssl libcurl4-openssl-dev build-essential git python-lzma python-crypto python3-crypto libqt4-opengl python-opengl python-qt4 python-qt4-gl python-numpy python-scipy libqt4-opengl python3-opengl python3-pyqt4 python3-pyqt4.qtopengl python3-numpy python3-scipy mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs squashfs-tools sleuthkit default-jdk lzop srecord zlib1g-dev liblzma-dev liblzo2-dev python-lzo xclip tmux screen minicom nasm xxd
+sudo apt install vim htop python-pip-whl python3-pip llvm libssl-dev cmake openssl libcurl4-openssl-dev build-essential git python-crypto python3-crypto python-opengl python-numpy libqt5opengl5 python3-opengl python3-numpy python3-scipy mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools sleuthkit default-jdk lzop srecord zlib1g-dev liblzma-dev liblzo2-dev xclip tmux screen minicom nasm xxd #cramfsprogs
 
 pip install capstone keystone-engine ropper unicorn pyqtgraph
 pip3 install capstone keystone-engine ropper unicorn pyqtgraph
